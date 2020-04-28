21a0abc4e75328703881fb3734a8c869c361cf13
matheusgoncalvs <matheusgoncalvs@hotmail.com>
2020-04-28 16:57:43 -0300

    adicionado o component para ser a area de menu

--- angular/src/generalframework/generalframework.module.ts
+++ angular/src/generalframework/generalframework.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { FrameworkBodyComponent } from './framework-body/framework-body.component';
 import { ContentComponent } from './content/content.component';
+import { TeuNavbarComponent } from './teu-navbar/teu-navbar.component';
 
 @NgModule({
   declarations: [
     FrameworkBodyComponent,
-    ContentComponent
+    ContentComponent,
+    TeuNavbarComponent
   ],
   imports: [
     CommonModule
---
+++ angular/src/generalframework/teu-navbar/teu-navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fw-teu-navbar',
+  templateUrl: './teu-navbar.component.html',
+  styleUrls: ['./teu-navbar.component.css']
+})
+export class TeuNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

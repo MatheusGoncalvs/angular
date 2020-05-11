0e6c4984fba9d46752017618d21e4f169d908899
matheusgoncalvs <matheusgoncalvs@hotmail.com>
2020-05-11 16:22:09 -0300

    Utilizando o serviço de ajuste de tela sem utilizar directives

--- angular/src/generalframework/teu-navbar/teu-navbar.component.ts
+++ angular/src/generalframework/teu-navbar/teu-navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ScreenService } from '../services/screen.service';
 
 @Component({
   selector: 'fw-teu-navbar',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeuNavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public screenService : ScreenService) { }
 
   ngOnInit(): void {
   }

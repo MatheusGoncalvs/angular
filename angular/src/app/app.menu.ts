78985974792d6224ac40087ad52f690b1d2ca0a9
matheusgoncalvs <matheusgoncalvs@hotmail.com>
2020-05-14 12:13:11 -0300

    Construido o aninhamento de dados ou objetos para popular os submenus
    + Gerado + 3 componentes

--- angular/src/app/app.menu.ts
+++ angular/src/app/app.menu.ts
@@ -9,14 +9,71 @@ export let initialMenuItems: Array<MenuItem> = [
     },
     {
         text: 'Countries',
-        icon: 'far fa-file-alt',
-        route: '/countries',
-        submenu: null
-    },
-    {
-        text: 'Settings',
-        icon: 'far fa-user-circle',
-        route: '/settings',
-        submenu: null
+        icon: 'glyphicon-flag',
+        route: null,
+        submenu: [
+            {
+                text: 'Select',
+                icon: 'glyphicon-expand',
+                route: null,
+                submenu: [
+                    {
+                        text: 'USA',
+                        icon: 'glyphicon-flag',
+                        route: 'country-detail/USA',
+                        submenu: null
+                    },
+                    {
+                        text: 'India',
+                        icon: 'glyphicon-flag',
+                        route: 'country-detail/India',
+                        submenu: null
+                    },
+                    {
+                        text: 'Brazil',
+                        icon: 'glyphicon-flag',
+                        route: 'country-detail/Brazil',
+                        submenu: null
+                    }
+                ]
+            },
+            {
+                text: 'Top 3',
+                icon: 'glyphicon-flag',
+                route: 'country-list/3',
+                submenu: null
+            },
+            {
+                text: 'Top 10',
+                icon: 'glyphicon-flag',
+                route: 'country-list/10',
+                submenu: null
+            },
+            {
+                text: 'All',
+                icon: 'glyphicon-flag',
+                route: 'country-list/0',
+                submenu: null
+            },
+            {
+                text: 'Maintenance',
+                icon: 'glyphicon-wrench',
+                route: null,
+                submenu: [
+                    {
+                        text: 'Country List',
+                        icon: 'glyphicon-th-list',
+                        route: 'country-maint',
+                        submenu: null
+                    },
+                    {
+                        text: 'Settings',
+                        icon: 'glyphicon-cog',
+                        route: 'settings',
+                        submenu: null
+                    }
+                ]
+            },
+        ]
     }
 ]
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -9,13 +9,19 @@ import { CountriesComponent } from './countries/countries.component';
 import { SettingsComponent } from './settings/settings.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.rounting';
+import { CountryListComponent } from './country-list/country-list.component';
+import { CountryMaintComponent } from './country-maint/country-maint.component';
+import { CountryDetailComponent } from './country-detail/country-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     CountriesComponent,
-    SettingsComponent
+    SettingsComponent,
+    CountryListComponent,
+    CountryMaintComponent,
+    CountryDetailComponent
   ],
   imports: [
     BrowserModule,
